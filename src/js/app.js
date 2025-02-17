01515e3da0e8aaa6cbeff8a860b1d4cba2c16563
Cecilia Manner <ceciliamannerr@gmail.com>
2025-02-17 21:02:50 +0100

    complete ui

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+
+
+// SELECT DOM ELEMENTS
+const openAddModalButton = document.querySelector(".add-books__button");
+const closeAddModalButton = document.querySelector(".form__cancel-button");
+const formModal = document.querySelector(".form-modal");
+const printedBookContainer = document.querySelector(".form__printed-book");
+const audioBookContainer = document.querySelector(".form__audio-book");
+
+// SELECTING FORM INPUTS
+const form = document.querySelector(".form");
+const title = document.querySelector(".form__title-input");
+const author = document.querySelector(".form__author-input");
+const publisher = document.querySelector(".form__publisher-input");
+const date = document.querySelector(".form__publication-input");
+const bookTypeDropdown = document.querySelector(".form__book-type");
+const filterContainer = document.querySelector(".filter-books");
+const formSubmitButton = document.querySelector(".form__add-button");
+const validationMessage = document.querySelector(".form__validation-message");
+
+// SELECTING ELEMENTS SPECIFIC TO PRINTED BOOKS
+const pages = document.querySelector(".form__pages-input");
+const printType = document.querySelector(".form__print-type");
+
+// SELECTING ELEMENTS SPECIFIC TO AUDIO BOOKS
+const narrator = document.querySelector(".form__narrator-input");
+const duration = document.querySelector(".form__duration-input");
+
+// ALL ELEMENTS IN PRINTED AND AUDIO CATEGORY
+const printedFields = [
+  document.querySelector(".form__pages-input"),
+  document.querySelector(".form__print-type"),
+];
+const audioFields = [
+  document.querySelector(".form__narrator-input"),
+  document.querySelector(".form__duration-input"),
+];
---
+++ src/js/ui.js
@@ -0,0 +1,50 @@
+
+
+class Ui {
+    static toggleBookTypeFields(
+        printedBookContainer,
+        audioBookContainer, 
+        audioFields, 
+        printedFields, 
+        bookType
+    ){
+        // HIDE BOTH CONTAINER INITALLY 
+        printedBookContainer.style.display = "none";
+        audioBookContainer.style.display = "none";
+        // RESET THE VALUES OF BOTH CATEGORIES
+        printedFields.forEach((field) => (field.value = ""));
+        audioFields.forEach((field) => (field.value = ""));
+        // DISPLAY THE RELEVANT CONTAINER BASED ON USER'S SELECTION
+        if (bookType === "printed-book") {
+        printedBookContainer.style.display = "block";
+        } else {
+        audioBookContainer.style.display = "block";
+        }
+    }
+    static displayAddModal(
+        openAddModalButton, 
+        formModal, 
+        printedBookContainer, 
+        audioBookContainer
+    ){
+        openAddModalButton.addEventListener("click", () => {
+            formModal.classList.add("display-form"); 
+            // HIDE BOTH CONTAINERS INITIALLY 
+            printedBookContainer.style.display = "none"; 
+            audioBookContainer.style.display = "none"; 
+        });
+    }
+    static closeAddModal(
+        closeAddModalButton,
+        formModal,
+        form,
+        validationMessage,
+        formSubmitButton
+      ){
+        closeAddModalButton.addEventListener("click", () => {
+            formModal.classList.remove("display-form"); 
+        });
+    }
+}
+
+export default Ui; 
